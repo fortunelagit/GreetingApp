9376e8fabc010d755f1188fe59cdc796069d2248
Khaela Fortunela <72397657+fortunelagit@users.noreply.github.com>
2022-03-01 03:38:54 +0700

    Add files via upload

---
+++ GreetingText/GreetingText/MainWindow.xaml.vb
@@ -0,0 +1,21 @@
+﻿Class MainWindow
+    Private Sub TextBox_TextChanged(sender As Object, e As TextChangedEventArgs)
+
+    End Sub
+
+    Private Sub TextBox_G(sender As Object, e As RoutedEventArgs)
+
+    End Sub
+
+    Private Sub GreetingTextBox_GotFocus(sender As Object, e As RoutedEventArgs) Handles GreetingTextBox.GotFocus
+        If (GreetingTextBox.Text = "Type Your Greeting Here") Then
+            GreetingTextBox.Text = ""
+        End If
+    End Sub
+
+    Private Sub SubmitGreetingButton_Click(sender As Object, e As RoutedEventArgs) Handles SubmitGreetingButon.Click
+        Dim Form2 As GreetingWindow = New GreetingWindow()
+        Form2.GreetingLabel.Content = GreetingTextBox.Text + "!"
+        Form2.Show()
+    End Sub
+End Class
---
+++ GreetingText/GreetingText/My Project/GreetingWindow.xaml.vb
@@ -0,0 +1,3 @@
+﻿Public Class GreetingWindow
+
+End Class
